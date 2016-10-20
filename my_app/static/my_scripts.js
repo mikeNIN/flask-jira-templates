1a4009684a088ffa1a9cb83564cf1b233a88fa49
mikenin <mikenin@mikenin-PC.(none)>
2016-10-20 16:03:04 +0200

    add AD module on front-end with AJAX search, on server search

---
+++ my_app/static/my_scripts.js
@@ -0,0 +1,15 @@
+/**
+ * Created by mikenin on 10/19/16.
+ */
+
+$(function() {
+    $('a#calculate').bind('click', function() {
+      $.getJSON($SCRIPT_ROOT + '/_add_numbers', {
+        a: $('input[name="a"]').val(),
+        b: $('input[name="b"]').val()
+      }, function(data) {
+        $("#result").text(data.result);
+      });
+      return false;
+    });
+  });
